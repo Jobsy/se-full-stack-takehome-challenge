b861edba57c56ef5786dca54dfb90fad6387599f
Oluwajoba Bello <oluwajobabello@gmail.com>
2023-05-22 09:44:44 +0100

    add favourite icon and logic

--- frontend-application/src/components/SaleCard/SaleCard.tsx
+++ frontend-application/src/components/SaleCard/SaleCard.tsx
@@ -1,28 +1,46 @@
 import * as React from "react";
 import { ISale } from "../../utils/Sale.interface";
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 import {
   DestenationText,
   SaleCardContent,
   SaleCardLink,
   TitleText,
 } from "./SaleCard.styles";
+import { FavouriteContext } from "../../context/FavouriteContext";
 
 interface ISaleCardProps {
   sale: Partial<ISale>;
 }
 
 export const SaleCard: React.FC<ISaleCardProps> = ({ sale }) => {
+
+  const { favourites, toggleFavourite } = React.useContext(FavouriteContext);
+
+  const isFavourite = favourites.includes(sale.id || "");
+  const userId = localStorage.getItem("SE_USER_ID");
+
   return (
-    <SaleCardLink to={`/sale/${sale.id}`}>
-      <img
-        width="100%"
-        src={sale.photos?.[0].url}
-        alt={sale?.editorial?.title}
-      />
-      <SaleCardContent>
-        <DestenationText>{sale?.editorial?.destinationName}</DestenationText>
-        <TitleText>{sale?.editorial?.title}</TitleText>
-      </SaleCardContent>
-    </SaleCardLink>
+    <div>
+      {userId && <FontAwesomeIcon
+        icon={faHeart}
+        color={isFavourite ? '#BB0039' : '#7E7E7E'}
+        onClick={() => toggleFavourite(sale.id || "")}
+      />}
+      
+      <SaleCardLink to={`/sale/${sale.id}`}>
+        <img
+          width="100%"
+          src={sale.photos?.[0].url}
+          alt={sale?.editorial?.title}
+        />
+        <SaleCardContent>
+          <DestenationText>{sale?.editorial?.destinationName}</DestenationText>
+          <TitleText>{sale?.editorial?.title}</TitleText>
+        </SaleCardContent>
+      </SaleCardLink>
+    </div>
   );
 };
