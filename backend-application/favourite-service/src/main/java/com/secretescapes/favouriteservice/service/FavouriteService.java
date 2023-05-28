b7ac0138c029f7d89c9e9b8011466aea9d708332
Oluwajoba Bello <oluwajobabello@gmail.com>
2023-05-29 00:03:48 +0100

    fix linter errors

--- backend-application/favourite-service/src/main/java/com/secretescapes/favouriteservice/service/FavouriteService.java
+++ backend-application/favourite-service/src/main/java/com/secretescapes/favouriteservice/service/FavouriteService.java
@@ -1,3 +1,93 @@
+//package com.secretescapes.favouriteservice.service;
+//
+//import com.secretescapes.favouriteservice.model.Favourite;
+//import com.secretescapes.favouriteservice.repository.FavouriteServiceRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.server.ResponseStatusException;
+//
+//import java.util.List;
+//import java.util.NoSuchElementException;
+//import java.util.Optional;
+//
+//
+//@Service
+//public class FavouriteService {
+//
+//    private FavouriteServiceRepository favouriteRepository;
+//
+//    @Autowired
+//    public FavouriteService(FavouriteServiceRepository favouriteRepository) {
+//        this.favouriteRepository = favouriteRepository;
+//    }
+//
+//    public Optional<Favourite> getFavoritesByUserIdAndSaleId(String userId, String saleId) {
+//        try {
+//            Optional<Favourite> existingFavourite = favouriteRepository.findByUserIdAndSaleId(userId, saleId);
+//            if (existingFavourite.isPresent()) {
+//                return existingFavourite;
+//            } else {
+//                throw new NoSuchElementException("Favorite not found");
+//            }
+//        } catch (IllegalArgumentException e) {
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid user or sale ID", e);
+//        } catch (Exception e) {
+//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to retrieve favorite", e);
+//        }
+//    }
+//
+//    public List<Favourite> getAllFavoritesByUserId(String userId) {
+//        try {
+//            List<Favourite> favourites = favouriteRepository.findByUserId(userId);
+//            if (favourites.isEmpty()) {
+//                throw new NoSuchElementException("Favorites not found");
+//            }
+//            return favourites;
+//        } catch (IllegalArgumentException e) {
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid user ID", e);
+//        } catch (NoSuchElementException e) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Favorites not found", e);
+//        }  catch (Exception e) {
+//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to retrieve favorites", e);
+//        }
+//    }
+//
+//    public ResponseEntity<String> addFavorite(String userId, String saleId) {
+//        try {
+//            return favouriteRepository.findByUserIdAndSaleId(userId, saleId)
+//                    .map(f -> ResponseEntity.ok("Favorite already exists"))
+//                    .orElseGet(() -> {
+//                        Favourite favourite = new Favourite(userId, saleId);
+//                        favouriteRepository.save(favourite);
+//                        return ResponseEntity.ok("Favorite added successfully");
+//                    });
+//        } catch (Exception e) {
+//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to add favorite", e);
+//        }
+//    }
+//
+//    public boolean removeFavorite(String userId, String saleId) {
+//        try {
+//            Optional<Favourite> existingFavourite = favouriteRepository.findByUserIdAndSaleId(userId, saleId);
+//
+//            if (existingFavourite.isPresent()) {
+//                favouriteRepository.delete(existingFavourite.get());
+//                return true;
+//            } else {
+//                throw new NoSuchElementException("Favorite not found");
+//            }
+//        } catch (IllegalArgumentException e) {
+//            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid user or sale ID", e);
+//        } catch (NoSuchElementException e) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Favorite not found", e);
+//        } catch (Exception e) {
+//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to remove favorite", e);
+//        }
+//    }
+//}
+
 package com.secretescapes.favouriteservice.service;
 
 import com.secretescapes.favouriteservice.model.Favourite;
@@ -12,18 +102,33 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
-
+/**
+ * Service class for managing favorites.
+ */
 @Service
 public class FavouriteService {
 
-    private FavouriteServiceRepository favouriteRepository;
+    private final FavouriteServiceRepository favouriteRepository;
 
+    /**
+     * Constructs a new FavouriteService with the given repository.
+     *
+     * @param favouriteRepository The repository for favorites.
+     */
     @Autowired
     public FavouriteService(FavouriteServiceRepository favouriteRepository) {
         this.favouriteRepository = favouriteRepository;
     }
 
-    public Optional<Favourite> getFavoritesByUserIdAndSaleId(String userId, String saleId) {
+    /**
+     * Retrieves the favorite by user ID and sale ID.
+     *
+     * @param userId The user ID.
+     * @param saleId The sale ID.
+     * @return The optional favorite.
+     * @throws ResponseStatusException If the favorite is not found or if there is an error retrieving it.
+     */
+    public Optional<Favourite> getFavoritesByUserIdAndSaleId(final String userId, final String saleId) {
         try {
             Optional<Favourite> existingFavourite = favouriteRepository.findByUserIdAndSaleId(userId, saleId);
             if (existingFavourite.isPresent()) {
@@ -38,7 +143,14 @@ public class FavouriteService {
         }
     }
 
-    public List<Favourite> getAllFavoritesByUserId(String userId) {
+    /**
+     * Retrieves all the favorites by user ID.
+     *
+     * @param userId The user ID.
+     * @return The list of favorites.
+     * @throws ResponseStatusException If the favorites are not found or if there is an error retrieving them.
+     */
+    public List<Favourite> getAllFavoritesByUserId(final String userId) {
         try {
             List<Favourite> favourites = favouriteRepository.findByUserId(userId);
             if (favourites.isEmpty()) {
@@ -49,12 +161,20 @@ public class FavouriteService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid user ID", e);
         } catch (NoSuchElementException e) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Favorites not found", e);
-        }  catch (Exception e) {
+        } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to retrieve favorites", e);
         }
     }
 
-    public ResponseEntity<String> addFavorite(String userId, String saleId) {
+    /**
+     * Adds a favorite for the given user ID and sale ID.
+     *
+     * @param userId The user ID.
+     * @param saleId The sale ID.
+     * @return A response entity indicating the result of the operation.
+     * @throws ResponseStatusException If there is an error adding the favorite.
+     */
+    public ResponseEntity<String> addFavorite(final String userId, final String saleId) {
         try {
             return favouriteRepository.findByUserIdAndSaleId(userId, saleId)
                     .map(f -> ResponseEntity.ok("Favorite already exists"))
@@ -68,20 +188,23 @@ public class FavouriteService {
         }
     }
 
-    public boolean removeFavorite(String userId, String saleId) {
+    /**
+     * Removes a favorite for the given user ID and sale ID.
+     *
+     * @param userId The user ID.
+     * @param saleId The sale ID.
+     * @return A response entity indicating the result of the operation.
+     * @throws ResponseStatusException If there is an error removing the favorite.
+     */
+    public ResponseEntity<String> removeFavorite(final String userId, final String saleId) {
         try {
             Optional<Favourite> existingFavourite = favouriteRepository.findByUserIdAndSaleId(userId, saleId);
-
             if (existingFavourite.isPresent()) {
                 favouriteRepository.delete(existingFavourite.get());
-                return true;
+                return ResponseEntity.ok("Favorite removed successfully");
             } else {
-                throw new NoSuchElementException("Favorite not found");
+                return ResponseEntity.notFound().build();
             }
-        } catch (IllegalArgumentException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid user or sale ID", e);
-        } catch (NoSuchElementException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Favorite not found", e);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to remove favorite", e);
         }
